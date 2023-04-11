7caf55b559537da9f80113678b0da35d39b9cf9d
Svetlana Zakharchenko <wxsxw2011@gmail.com>
2023-04-11 21:36:15 +0300

    01-1

--- js/scriptGreg_01.js
+++ js/scriptGreg_01.js
@@ -194,15 +194,15 @@
 // - 1441 покажет 24:01
 
 // ```js
-const totalMinutes = 1441;
+// const totalMinutes = 1441;
 
-const hours = Math.floor(totalMinutes / 60);
-const minutes = totalMinutes % 60;
-console.log(hours);
-console.log(minutes);
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
 
-const doubleDigitHours = String(hours).padStart(2, 0);
-const doubleDigitMinutes = String(minutes).padStart(2, 0);
-console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
 // ```
 //
